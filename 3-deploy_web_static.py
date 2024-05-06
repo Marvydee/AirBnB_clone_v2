46a465124a7679e7a5e0a192df0c9279ba3a3723
Marvydee <marvydee1@gmail.com>
2024-05-06 22:55:14 +0100

    deploy web static

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Create and distributes an archive to web servers"""
+"""Create and distributes an archive to my web servers"""
 import os.path
 import time
 from fabric.api import local
